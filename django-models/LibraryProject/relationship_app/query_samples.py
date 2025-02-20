165c7d2dd36096e1fa749e65354541910b121121
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-20 14:20:00 +0100

    Query Samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,2 +1,20 @@
-Book.objects.all().values()
-Book.objects.get()
+from relationship_app.models import Library, Book, Author, Librarian
+
+author_name = 'filler'
+
+author = Author.objects.get(name = author_name)
+books_by_author = Book.objects.filter(author = author)
+print(f"Books by {author_name}:")
+for book in books_by_author:
+    print(book.title)
+
+
+library_name = 'empty'
+
+library = Library.objects.get(name = library_name)
+books_in_library = Book.objects.filter(library = library)
+
+print(f"Books in {library_name}:")
+for book in books_in_library:
+    print(book.title)
+
