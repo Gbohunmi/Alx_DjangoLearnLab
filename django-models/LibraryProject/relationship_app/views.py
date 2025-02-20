fc1c1fc69007e4d14f805e0f4d5857af369ccbbc
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-20 14:31:21 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def list_books(request):
+      """Retrieves all books and renders a template displaying the list."""
+      books = Book.objects.all()  # Fetch all book instances from the database
+      context = {'book_list': books}  # Create a context dictionary with book list
+      return render(request, 'books/list_books.html', context)
