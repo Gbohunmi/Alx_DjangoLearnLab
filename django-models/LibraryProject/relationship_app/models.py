9657b1a6b021c5b4bbabb7c898470789ff82c5cc
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-20 13:58:02 +0100

    Removed space in class definition of Librarian

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    return self.name 
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -13,7 +15,7 @@ class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='library')
 
-class Librarian (models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     
