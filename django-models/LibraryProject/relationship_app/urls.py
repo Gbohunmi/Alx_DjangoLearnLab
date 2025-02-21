1fd65e49668117d0340cc13dbdeddfeff6d096fa
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-21 11:36:45 +0100

    Removed spaces

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('books/', views.list_books, name='books'),
     path('library/', views.LibraryListView.as_view(), name='library-list'),
-    path("login/", auth_views.LoginView.as_view(template_name = 'login.html'), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name = 'logout.html'), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
     path("register/", auth_views.register, name="register") 
 ]
