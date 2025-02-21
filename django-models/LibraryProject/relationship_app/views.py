ac1a45f72a8bba6972a04a2678633ccc5782af97
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-21 12:42:33 +0100

    Capitalised User

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -63,14 +63,14 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-def is_admin(user):
-    return user.userprofile.role == "Admin"
+def is_admin(User):
+    return User.UserProfile.role == "Admin"
 
-def is_librarian(user):
-    return user.userprofile.role == "Librarian"
+def is_librarian(User):
+    return User.UserProfile.role == "Librarian"
 
-def is_member(user):
-    return user.userprofile.role == "Member"
+def is_member(User):
+    return User.UserProfile.role == "Member"
 
 @user_passes_test(is_admin)
 def admin_view(request):
