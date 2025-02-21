4d9e3b5598d18e5794b385126e144b04613a64b3
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-21 12:41:02 +0100

    Changed to double apostrophe

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -64,13 +64,13 @@ from .models import UserProfile
 
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.userprofile.role == "Admin"
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.userprofile.role == "Librarian"
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.userprofile.role == "Member"
 
 @user_passes_test(is_admin)
 def admin_view(request):
