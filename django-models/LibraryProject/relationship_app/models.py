06f5778703f56917cef6fb85198a8fbc0491642c
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-02-21 12:39:21 +0100

    Removed Default Role

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -42,6 +42,6 @@ class UserProfile(models.Model):
 
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=9, choices=roles, default="Member")
+    role = models.CharField(max_length=9, choices=roles)
 
     
