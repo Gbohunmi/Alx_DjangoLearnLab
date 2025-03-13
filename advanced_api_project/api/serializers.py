4d4ff1fc1d70f1a169d2520ffe7ed1e57b6010f2
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-13 21:36:20 +0100

    Created Custom Serializers

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,2 +1,20 @@
-from rest_framework import serializers 
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import datetime
 
+#Defined Book Serializer and validated the Publication year
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['__all__']
+
+    def validate(self, publication_year):
+        if datetime.now().year < publication_year:
+            raise serializers.ValidationError(f'{publication_year} is in the future')
+        return super().validate(publication_year)
+
+#Defined Author Serializer
+class AuthorSerializer(serializers.ModelSerializer):
+     class Meta:    
+        model = Author
+        fields = ['name']
