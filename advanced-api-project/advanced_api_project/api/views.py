d2a8cf71f1e5295f609318fe476839d814f9147a
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-13 22:05:41 +0100

    Adjust Directories again

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.generics import (ListAPIView, 
+                                     CreateAPIView,
+                                     RetrieveAPIView,
+                                     UpdateAPIView, 
+                                     DestroyAPIView
+)
 # Create your views here.
+
+class ListView(ListAPIView):
+    
