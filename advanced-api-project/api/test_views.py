9dd3b2d8c560cebd75a23b74445cfd6dd15b9ebb
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-14 15:08:48 +0100

    Created BookAPITests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 from .models import Book
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+
+class BookAPITests(APITestCase):
+    #Setting up of the required tests
+
+    def SetUp(self):
+        self.user = User.objects.create_user(username='test_user', password='test123#')
+        self.client.login(username="test_user", password="test123")
+       
+        self.book = Book.objects.create(title='Test_Book', publication_year=2019, author='Orwell')
+        
+    def test_get_books(self):
+        # Testing if an authenticated user can get the list of books
+        response = self.client.get("/api/books/")
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_unauthenticated_user(self):
+        # Check if an authenticated user can access the list of books
+        self.client.logout()
+        response = self.client.get("/api/books/")
+        self.assertEqual(response.status_code, 200)  
+
+    def test_unauthenticated_user_create(self):
+        # Check if an authenticated user can create books.
+        self.client.logout()
+        response = self.client.get("/api/books/create")
+        self.assertEqual(response.status_code, 401)  
