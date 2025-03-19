165c974c3c10a18aa7aebe90e9f613a5e68dbb73
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-19 14:54:10 +0100

    Changed URL patterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
 
     #Post Views
     path("posts/new", PostCreateView.as_view(template_name="post_form.html"), name="create-post"),
-    path("posts", PostListView.as_view(template_name="post_form.html"), name="posts"),
+    path("posts", PostListView.as_view(template_name="post_list.html"), name="posts"),
     path("posts/<int:pk>", PostCreateView.as_view(template_name="post_detail.html"), name="post-details"),
-    path("/posts/<int:pk>/edit/", PostCreateView.as_view(template_name="edit_post.html"), name="edit-post"),
+    path("/posts/<int:pk>/update/", PostCreateView.as_view(template_name="edit_post.html"), name="edit-post"),
     path("/posts/<int:pk>/delete/", PostCreateView.as_view(template_name="confirm_delete.html"), name="delete-post"),
 ]
