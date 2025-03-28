a449fcf6dfd857e9b5485592780bbccb49c0a05f
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-28 15:19:00 +0100

    Created serializers, views and routers for posts and comments

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] #Allows any user to view posts.
+
+    def perform_create(self, serializer):
+        # Automatically sets the current user as the author when creating a post.
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]#Allows only authenticated users to view posts.
+
+
+    def perform_create(self, serializer):
+        # Automatically sets the current user as the author when creating a comment.
+        serializer.save(author=self.request.user)
+
